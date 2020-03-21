0c382a7551a09e0b58a1892a3d1974291b5941d6
langej <langej@users.noreply.github.com>
2020-03-21 16:34:34 +0100

    added filtered output of maven

--- maverick.sh
+++ maverick.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+overwrite_line() { echo -e "\r\033[1A\033[0K$@"; }
+
 compile() {
-echo ">> compile to xml"
+echo "[1/2] compile to xml"
 file=`cat $PWD/pom.pug`
 text="mixin dependency(group, artifact, version)
 	dependency
@@ -15,8 +17,23 @@ echo "$text" | pug -Ps > pom.xml
 }
 
 start_maven() {
-	echo ">> start maven"
-	mvn -f $PWD/pom.xml clean install
+	overwrite_line "[2/2] run maven"
+	echo -e '\r'
+	while read -r line
+	do
+		if [[ $line == *"ERROR"* ]]; then
+			echo -e "\033[0;31m$line\r"
+		elif [[ $line == *"BUILD SUCCESS"* ]]; then
+			echo -e "\033[0;32m$line\n"
+		elif [[ $line == *"Total time"* ]]; then
+			echo -e "\033[0m$line\r"
+		elif [[ $line == *"Finished at"* ]]; then
+			echo -e "\033[0m$line\n"
+		else
+			overwrite_line "$line"
+		fi
+	done < <(mvn -f $PWD/pom.xml clean install)
+	echo -e "[[ finshed work! ]]"
 }
 
 check_requirements() {
@@ -55,5 +72,5 @@ fi
 if [[ -e "$PWD/pom.xml" ]]; then
 	start_maven
 else
-	echo "need a pom.xml to start maven..."
+	echo "need a pom.xml to run maven..."
 fi
